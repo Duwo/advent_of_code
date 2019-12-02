ced93bb4303d45e0e378a4f51951e5b26da308d1
Erik Rodhe <erikrodhe@gmail.com>
2019-12-02 23:25:16 +0100

    Day 2 Part 2

--- 2/intercode.py
+++ 2/intercode.py
@@ -1,4 +1,4 @@
-import os
+import os, copy
 
 def read_code():
     file = os.path.dirname(os.path.abspath(__file__)) + '/data/input'
@@ -7,40 +7,53 @@ def read_code():
     return intercode
 
 
-def main():
-    # intercode = [1, 0, 0, 0, 99]
-    # intercode = [1,1,1,4,99,5,6,0,99]
-    intercode = read_code()
-    print(intercode)
-    print("-----------------")
-    intercode[1] = 12
-    intercode[2] = 2
+def run_intercode(intercode):
     length = len(intercode)
-
-    for i in range(length):
-        if not (i == 0 or i % 4 == 0):
+    for instructionPointer in range(length):
+        if not (instructionPointer == 0 or instructionPointer % 4 == 0):
             continue
 
-        if intercode[i] == 99:
+        if intercode[instructionPointer] == 99:
             return intercode
 
-        optcode = intercode[i]
-        inputPosition1 = intercode[i+1]
-        inputPosition2 = intercode[i+2]
-        outputPosition = intercode[i+3]
+        optcode = intercode[instructionPointer]
+        inputParameter1 = intercode[instructionPointer+1]
+        inputParameter2 = intercode[instructionPointer+2]
+        outputParameter = intercode[instructionPointer+3]
 
         if optcode == 1:
             try :
-                intercode[outputPosition] = intercode[inputPosition1] + intercode[inputPosition2]
+                intercode[outputParameter] = intercode[inputParameter1] + intercode[inputParameter2]
             except:
                 pass
         elif optcode == 2:
             try:
-                intercode[outputPosition] = intercode[inputPosition1] * intercode[inputPosition2]
+                intercode[outputParameter] = intercode[inputParameter1] * intercode[inputParameter2]
             except:
                 pass
 
     return intercode
+
+
+
+def main():
+    # intercode = [1, 0, 0, 0, 99]
+    # intercode = [1,1,1,4,99,5,6,0,99]
+    intercode = read_code()
+    memory = copy.copy(intercode)
+    intercode[1] = 12
+    intercode[2] = 2
+
+    # return run_intercode(intercode)
+    for i in range(1, 100):
+        intercode[1] = i
+        for j in range(1, 100):
+            intercode[2] = j
+            if run_intercode(intercode)[0] == 19690720:
+                return 100*i + j
+            else:
+                intercode = copy.copy(memory)
+                intercode[1] = i
 
 if __name__ == "__main__":
     print(main())
