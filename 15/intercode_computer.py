ed58fccd904e13c3be85c7262be5bcd36fe97006
Erik Rodhe <erikrodhe@gmail.com>
2019-12-23 01:10:09 +0100

    Day 15 Part something

--- 15/intercode_computer.py
+++ 15/intercode_computer.py
@@ -2,114 +2,108 @@ import os, copy
 import random
 from amplifier import Amplifier
 
+
 class IntercodeComputer():
     def __init__(self, intercode, amp_mode, feedback_mode):
         self.intercode = intercode
-        self.code_runners = []
-        self.output = ''
+        self.code_runners = None
+        self.oxygen = None
         self.grid = [[' ']*100 for i in range(100)]
         self.position = [50, 50]
-        self.explored_walls = []
-        self.explored_empty = []
+        self.possible_positions = [[[50,50]]]
         self.target = [38, 68]
-        self.steps = 0
+        self.steps = 1
+        self.current_depth = 0
 
 
     def create_droid(self):
-        self.code_runners.append(Amplifier([], self.intercode, self))
+        self.code_runner = Amplifier([], self.intercode, self)
         self.grid[self.position[1]][self.position[0]] = 'S'
 
-    def reset_memory(self, setting_combination=[]):
-        self.amplifiers = []
-
     def run(self, input_signal):
-        done = False
-        amp = self.code_runners[0]
-        while not done:
-            amp.run_code(input_signal)
-            done, input_signal = self.handle_status_code(input_signal, amp.output)
-        # while not done:
-        #     amp.run_code(input_signal)
-        #     done, input_signal = self.handle_status_code(input_signal,
-        #                                                  amp.output
-        #                                                  target)
+        steps = 1
+        while not self.oxygen:
+            current_path = [copy.copy(self.position)]
+            self.explore(current_path)
+            self.steps += 1
 
-        return done
+        return self.steps
+
+    def explore(self, current_path):
+        if len(current_path) == self.steps:
+            self.explore_new()
+        else:
+            print(self.possible_positions)
+            for position in self.possible_positions[self.steps-1]:
+                self.code_runner.run_code(position[0])
+                self.position = position[1]
+                current_path.append(position)
+                self.explore(current_path)
+                position =  current_path.pop()
+                self.code_runner.run_code(self.revert_input(position[0]))
+                self.position = position[1]
+
+
+    def explore_new(self):
+        input_signals = [signal for signal in range(1,5)]
+        for input_signal in input_signals:
+            self.code_runner.run_code(input_signal)
+            self.handle_status_code(self.code_runner.output, input_signal)
 
     def next_position(self, input_signal):
         if input_signal == 1:
-            return [self.position[0], self.position[1] + 1]
+            return (1, [self.position[0], self.position[1] + 1])
         elif input_signal == 2:
-            return [self.position[0], self.position[1] - 1]
+            return (2,[self.position[0], self.position[1] - 1])
         elif input_signal == 3:
-            return [self.position[0] - 1, self.position[1]]
+            return (3, [self.position[0] - 1, self.position[1]])
         elif input_signal == 4:
-            return [self.position[0] + 1, self.position[1]]
+            return (4, [self.position[0] + 1, self.position[1]])
 
-    def next_movement(self):
-        possible_nexts = \
-            [self.next_position(1),
-             self.next_position(2),
-             self.next_position(3),
-             self.next_position(4)]
-        directions = [i for i in range(4)]
+    def revert_input(self, input_signal):
+        if input_signal == 1:
+            return 2
+        elif input_signal == 2:
+            return 1
+        elif input_signal == 3:
+            return 4
+        elif input_signal == 4:
+            return 3
 
-        for i, next_pos in enumerate(possible_nexts):
-            if next_pos in self.explored_walls:
-                directions.remove(i)
+    def add_to_positions(self, next_position):
+        if len(self.possible_positions) < self.steps + 1:
+            self.possible_positions.append([])
 
-        backtrack = []
-        for i, next_pos in enumerate(possible_nexts):
-            if next_pos in self.explored_empty:
-                directions.remove(i)
-                backtrack.append(i)
-
-        possible_nexts_no_walls = [possible_nexts[i] for i in directions]
-        possible_backtracks_no_walls = [possible_nexts[i] for i in backtrack]
-
-        if directions:
-            # if self.target:
-            #     best_next = self.get_minimum_to_target(possible_nexts_no_walls)
-            #     self.steps += 1
-                # return possible_nexts.index(best_next) + 1
-            direction = random.randint(0, len(directions)-1)
-            self.steps += 1
-            return directions[direction] + 1
+        if self.already_added(next_position) or self.going_back(next_position):
+            pass
         else:
-            # if self.target:
-            #     best_next = self.get_minimum_to_target(possible_backtracks_no_walls)
-            #     self.steps += 1
-            #     return possible_nexts.index(best_next) + 1
-            direction = random.randint(0, len(backtrack)-1)
-            self.steps -= 1
-            return backtrack[direction] + 1
+            self.possible_positions[self.steps].append(next_position)
 
-    def get_minimum_to_target(self, next_positions):
-        sorted_nexts = sorted(
-            next_positions,
-            key=lambda pos: abs(pos[0]-self.target[0]) + abs(pos[1] - self.target[1])
-        )
-        return sorted_nexts[0]
+    def already_added(self, next_position):
+        return next_position in self.possible_positions[self.steps]
 
-    def handle_status_code(self, input_signal, output):
-        next_position = copy.copy(self.next_position(input_signal))
-        done = False
+    def going_back(self, next_position):
+        if len(self.possible_positions) < 3:
+            return False
+        return next_position in self.possible_positions[self.steps - 2]
+
+    def handle_wall(self, next_position):
+        self.grid[next_position[1][1]][next_position[1][0]] = '#'
+
+    def handle_empty(self, next_position, input_signal):
+        self.grid[next_position[1][1]][next_position[1][0]] = '.'
+        self.add_to_positions(next_position)
+
+    def hanlde_oxygen_tank(self, next_position):
+        self.grid[next_position[1][1]][next_position[1][0]] = 'O'
+        self.oxygen = next_position
+
+    def handle_status_code(self, output, input_signal):
+        next_position = self.next_position(input_signal)
         if output == 0:
-            self.grid[next_position[1]][next_position[0]] = '#'
-            self.explored_walls.append(next_position)
-            input_signal = self.next_movement()
+            self.handle_wall(next_position)
         elif output == 1:
-            self.grid[next_position[1]][next_position[0]] = '.'
-            self.explored_empty.append(next_position)
-            self.position = next_position
-            input_signal = self.next_movement()
+            self.handle_empty(next_position, input_signal)
         elif output == 2:
-            self.grid[next_position[1]][next_position[0]] = 'O'
-            print('Found oxygen at: ', next_position)
-            done = True
-            input_signal = None
-
-        return done, input_signal
-
-
+            self.handle_oxygen_tanke(next_position)
 
